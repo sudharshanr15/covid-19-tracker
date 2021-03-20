171725334454fa6d70d65526c4c3738a98c93fb2
Sudharshan <sudharshanrms2004@gmail.com>
2021-03-20 19:03:45 +0530

    added routes

--- public/index.php
+++ public/index.php
@@ -9,5 +9,4 @@ $app->router->get('/India/dailyrecords', "dailyrecords");
 $app->router->get("/India/state/karnataka", "KA");
 $app->router->get("/India/state/Tamilnadu", "TN");
 $app->router->get("/India/state/Maharashtra", "MH");
-// $app->router->get("/India/statewise/karnataka", "statewise");
 $app->run();
